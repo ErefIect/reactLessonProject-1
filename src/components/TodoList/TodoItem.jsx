2391ac6723125b01e2b43a390f9021de7b5526cd
ErefIect <luodan_0464@qq.com>
2023-04-18 21:39:24 +0800

    modify format

--- src/components/TodoList/TodoItem.jsx
+++ src/components/TodoList/TodoItem.jsx
@@ -2,10 +2,12 @@ import { useCallback } from "react";
 import "./TodoItem.css";
 
 function TodoItem({ index, items, setMessages, contents }) {
+
   const deleteItem = useCallback(() => {
     const newItems = items.filter((item, i) => i !== index);
     setMessages(newItems);
   }, [index, items, setMessages]);
+
   return (
     <div className="todo-item">
       <div>{contents}</div>
