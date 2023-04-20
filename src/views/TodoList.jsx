ea793eacdfb8d1a599965f24a3084b4fe2445853
ErefIect <luodan_0464@qq.com>
2023-04-20 12:00:31 +0800

    use sideEffect

--- src/views/TodoList.jsx
+++ src/views/TodoList.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ListRender from "../components/TodoList/ListRender";
 import NewItem from "../components/TodoList/NewItem";
 
 function TodoList() {
   const [messages, setMassages] = useState([]);
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((data) => {
-      data = data.slice(0, 10).map((e) => e.body.slice(0, 10));
-      setMassages(data);
-    })
-    .catch((err) => console.log(err));
-
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        data = data.slice(0, 10).map((e) => e.body.slice(0, 10));
+        setMassages(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <>
       <ListRender messages={messages} setMessages={setMassages} />
